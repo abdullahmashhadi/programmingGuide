1d0ff04f7e01ddd9a3e9e62f8db94156073a04ce
abdullahmashhadi <abdullah6blue@gmail.com>
2024-07-03 10:48:06 +0500

    original project

--- app.py
+++ app.py
@@ -49,36 +49,6 @@ def chat_with_pdf(source_id, user_message):
         print('Error:', response.text)
         return None
 
-def text_to_speech(text):
-    api_key = os.getenv('TTS_API_KEY')
-    if not api_key:
-        raise ValueError("No TTS API key found. Please set the TTS_API_KEY environment variable in the .env file.")
-
-    headers = {
-        'Authorization': f'Bearer {api_key}',
-        "Content-Type": "application/json",
-    }
-
-    data = {
-        "input": {"text": text},
-        "voice": {"languageCode": "en-US", "name": "en-US-Wavenet-D"},
-        "audioConfig": {"audioEncoding": "MP3"}
-    }
-    response = requests.post(
-        'https://texttospeech.googleapis.com/v1/text:synthesize', headers=headers, json=data)
-
-    if response.status_code == 200:
-        audio_content = response.json()['audioContent']
-        with open('static/response.mp3', 'wb') as audio_file:
-            audio_file.write(audio_content.encode('latin1'))
-        return 'static/response.mp3'
-    else:
-        print('Status:', response.status_code)
-        print('Error:', response.text)
-        return None
-
-
-
 @app.route('/')
 def index():
     return send_from_directory(app.static_folder, 'index.html')
@@ -95,9 +65,6 @@ def chat():
 
     answer = chat_with_pdf(source_id, user_input)
     if answer:
-        audio_path = text_to_speech(answer)
-        return jsonify({'answer': answer, 'audioPath': audio_path})
+        return jsonify({'answer': answer})
     else:
         return jsonify({'answer': 'An error occurred while processing your request.'}), 500
-
-
